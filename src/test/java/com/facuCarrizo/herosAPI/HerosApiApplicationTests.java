02671c481d6dc768996003812e6797126d58e7ad
facundocarrizo99 <49492465+facundocarrizo99@users.noreply.github.com>
2022-04-01 01:43:39 -0300

    Web Service OK

---
+++ src/test/java/com/facuCarrizo/herosAPI/HerosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facuCarrizo.herosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
